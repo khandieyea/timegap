7ec66b3b4c9717195c6ddb1a53bdc135c67f36eb
Warren <w@uber.co.nz>
2013-07-22 14:43:48 +1200

    A

--- tests/tests.php
+++ tests/tests.php
@@ -6,8 +6,8 @@ class Tests extends PHPUnit_Framework_TestCase
     public function testSomething()
     {	
  	
- 		$this->assertEquals(100, 100);      
- 		
+ 		$this->assertEquals(102, 100);      
+
     }
 
 }
