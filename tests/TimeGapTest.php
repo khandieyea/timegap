61038c8d0575bce2ce59d7420c69b3d20944b407
Warren <w@uber.co.nz>
2013-07-22 14:59:09 +1200

    trying to call class from phpunit

--- tests/TimeGapTest.php
+++ tests/TimeGapTest.php
@@ -5,6 +5,9 @@ class Tests extends PHPUnit_Framework_TestCase
     public function testSomething()
     {	
  	
+
+    	$x = new TimeGap\TimeGap();
+
  		$this->assertEquals(102, 100);      
  		
  		
